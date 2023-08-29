810fef270989af72f7cdb0b477141310c4b75f87
ssoymilk <kmmm3@naver.com>
2023-08-29 17:37:04 +0900

    20230829

--- HelloReact/h20230828.js
+++ HelloReact/h20230828.js
@@ -380,7 +380,7 @@ class Vacation {
 const trip = new Vacation("칠레 산티아고", 9);
 
 trip.print();
-*/
+/*
 // 상속
 
 class Vacation {
@@ -411,3 +411,288 @@ class Expedition extends Vacation {
 const trip = new Expedition("한라산", 3, ["선글라스", "오색 깃발", "카메라"]);
 
 trip.print();
+*/
+/*
+var log = function (message) {
+  console.log(message);
+};
+
+log("자바스크립트에서는 함수를 변수에 넣을 수 있습니다.");
+
+const log2 = (message) => console.log(message);
+
+log2("ES6 화살표 함수");
+
+const obj = {
+  message: "함수를 다른 값과 마찬가지로 객체에 추가할 수도 있습니다.",
+  log() {
+    console.log(this.message);
+  },
+};
+
+obj.log();
+const messages = [
+  "함수를 배열에 넣을 수도 있습니다.",
+  (message) => console.log(message),
+  "일반적인 값과 마찬가지입니다.",
+  (message) => console.log(message),
+];
+
+messages[1](messages[0]);
+messages[3](messages[2]);
+
+const insideFn = (logger) =>
+logger("함수를 다른 함수에 인자로 넘길 수도 있습니다.");
+
+insideFn((message) => console.log(message));
+
+var createScream = (logger) => (m) =>
+logger(m.toUpperCase() + "!!!");
+
+const scream = createScream((message) => console.log(message));
+
+scream("함수가 함수를 반환할 수도 있습니다");
+scream("createScream은 함수를 반환합니다");
+scream("scream은 createScream이 반환한 함수를 가리킵니다");
+
+const string = "Restaurants in Hanalei";
+let urlFriendly = "";
+
+for (let i = 0; i < string.length; i++) {
+  if (string[i] === " ") {
+    urlFriendly += "-";
+  } else {
+    urlFriendly += string[i];
+  }
+}
+
+// urlFriendly = urlFriendly.toLowerCase();
+
+console.log(urlFriendly);
+
+const string = "Restaurants in Hanalei";
+let urlFriendly = string.replace(/ /g, "-"); // g = global
+
+console.log(urlFriendly);
+
+var target = document.getElementById("target");
+var wrapper = document.createElement("div");
+var headLine = document.createElement("h1");
+
+wrapper.id = "welcome";
+headLine.innerText = "Hello World";
+
+wrapper.appendChild(headLine);
+target.appendChild(wrapper);
+
+// 위에걸 리액트라면
+const { render } = ReactDOM;
+
+const Welcome = () => (
+  <div id="welcome">
+    <h1>Hello World</h1>
+  </div>
+);
+
+render(<Welcome />, document.getElementById("target"));
+
+const color_lawn = {
+  title: "잔디",
+  color: "#00FF00",
+  rating: 0,
+};
+function rateColor(color, rating) {
+  color.rating = rating;
+  return color;
+}
+console.log(rateColor(color_lawn, 5).rating);
+console.log(color_lawn.rating);
+
+let color_lawn_1 = rateColor(color_lawn, 5);
+console.log(Object.is(color_lawn, color_lawn_1));
+color_lawn_1 = {
+  title: "잔디",
+  color: "#00FF00",
+  rating: 1,
+};
+console.log(Object.is(color_lawn, color_lawn_1));
+
+var rateColor1 = function (color, rating) {
+  return Object.assign({}, color, { rating: rating });
+};
+
+//
+// Object.assign으로 복사본을 만들어서 평점을 부여한다
+//
+
+console.log(rateColor1(color_lawn, 5).rating);
+console.log(color_lawn.rating);
+
+const rateColor2 = (color) => ({
+  ...color,
+  rating,
+});
+*/
+/*
+let list = [
+  { title: "과격한 빨강" },
+  { title: "잔디" },
+  { title: "파티 핑크" },
+];
+// 원본 바꿈
+const addColor = (qwe, asd) => qwe.push({ asd });
+console.log(addColor(list, "화려한 녹색").length);
+console.log(list);
+
+console.log(list.length);
+
+// 원본 유지
+const addColor2 = (title, array) => [...array, { title }];
+console.log(addColor2("갈색", list).length);
+console.log(list.length);
+
+// 순수 함수
+const frederick = {
+  name: "frederick Douglass",
+  canRead: false,
+  canWrite: false,
+};
+
+const selfEd = (a, canRead, canWrite) => ({ ...a, canRead, canWrite });
+console.log(selfEd(frederick, true, true));
+
+function selfEducate(person) {
+  frederick.canRead = true;
+  frederick.canWrite = true;
+  return person;
+}
+
+selfEducate();
+console.log(frederick);
+
+//
+let schools = ["Yorktown", "Washington & Lee", "Wakefield"];
+console.log(schools.join(", "));
+
+const wSchools = schools.filter((asd) => asd[0] === "W");
+
+console.log(wSchools);
+
+const cutSchool = (cut, list) => list.filter((want) => want !== cut);
+console.log(cutSchool("Washington & Lee", schools).join("\n"));
+console.log(schools.join("\n"));
+
+const highSchools = schools.map((school) => `${school} High School`);
+console.log(highSchools.join("\n"));
+
+const highSchools1 = (a) => a.map((school) => `${school} High School`);
+console.log(highSchools1(schools).join("\n"));
+
+schools = [
+  { name: "Yorktown" },
+  { name: "Stratford" },
+  { name: "Washington" },
+  { name: "Wakefield" },
+];
+
+const editName = (oldName, name, arr) =>
+  arr.map((item) => (item.name === oldName ? { ...item, name } : item));
+console.log(schools);
+console.log(editName("Stratford", "abc", schools));
+
+const editNth = (n, name, arr) =>
+  arr.map((item, i) => (i === n ? { ...item, name } : item));
+console.log(schools);
+console.log(editNth(2, "Stratford", schools));
+
+const addSchool = (a, name) => a.concat({ name });
+console.log(addSchool(schools, "서울대학교"));
+
+console.log(schools);
+
+const filterSchool = (a, cut) => console.log(a.filter((name) => name !== cut));
+filterSchool(schools, "Washington");
+
+const schools = {
+  "Yorktown": 10,
+  "Washington & Lee": 2,
+  "Wakefield": 5,
+}
+const schoolArray = Object.keys(schools).map(key => ({
+  name: key,
+  wins: schools[key]
+}))
+console.log(schoolArray);
+console.log(Object.keys(schools));
+console.log(schools);
+
+const ages = [21, 18, 42, 40, 64, 63, 34];
+const sumAge = ages.reduce((p0, p1) => p0 + p1) / ages.length;
+console.log(sumAge);
+
+const maxAge = ages.reduce((max, age) => (age > max ? age : max));
+const minAge = ages.reduce((max, age) => (age < max ? age : max));
+console.log(maxAge);
+console.log(minAge);
+
+const colors = [
+  {
+    id: "-xekare",
+    title: "과격한 빨강",
+    rating: 3,
+  },
+  {
+    id: "-jbwsof",
+    title: "큰 파랑",
+    rating: 2,
+  },
+  {
+    id: "-prigbj",
+    title: "큰곰 회색",
+    rating: 5,
+  },
+  {
+    id: "-ryhbhsl",
+    title: "바나나",
+    rating: 1,
+  },
+];
+
+const hashColors = colors.reduce(
+  (hash, {id, title, rating}) => {
+    hash[id] = {title, rating}
+    return hash;
+  },
+  {}
+  )
+  console.log(hashColors);
+
+const colors = ["red", "red", "green", "blue", "green"];
+const uniqueColors = colors.reduce(
+  (unique, color) =>
+    unique.indexOf(color) !== -1 ? unique : [...unique, color],
+  []
+);
+console.log(uniqueColors);
+
+const invokeIf = (condition, fnTrue, fnFalse) =>
+  condition ? fnTrue() : fnFalse();
+
+invokeIf(
+  true,
+  () => console.log("Welcome"),
+  () => console.log("Unauthorize")
+);
+
+const userLogs = (userName) => (message) =>
+  console.log(`${userName} ${message}`);
+
+const log = userLogs("grandpa");
+log("hello");
+*/
+
+const countDown = (value, fn) => {
+  fn(value);
+  return value > 0 ? countDown(value - 1, fn) : value;
+};
+countDown(10, (value) => console.log(value));
